77e285ae11ac88bfcfda337b5ebdd3550bc51516
Martin Andreev <xxdead_worldxx@abv.bg>
2024-02-10 10:42:35 +0200

    Declare path for single post
    This path was used, but wasn't declared in the routing. Declare it in the routing in order to fix the navigation to the single post.

--- forum/forum/src/App.jsx
+++ forum/forum/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./config/firebase-config";
 import "./App.css";
 import Home from "./views/Home/Home";
 import NotFound from "./views/NotFound/NotFound";
@@ -12,6 +13,7 @@ import CreatePost from "./views/CreatePost/CreatePost";
 import { AppContext } from "./Context/AppContext"; //???
 import Footer from "./components/Footer/Footer";
 import AllPosts from "./views/AllPosts/AllPosts";
+import SinglePost from "./views/SinglePost/SinglePost";
 
 function App() {
   const [context, setContext] = useState({
@@ -30,6 +32,7 @@ function App() {
           <Route path="/Latest" element={<Latest />} />
           <Route path="/trending" element={<Trending />} />
           <Route path="/allposts" element={<AllPosts />} />
+          <Route path="/posts/:id" element={<SinglePost />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
