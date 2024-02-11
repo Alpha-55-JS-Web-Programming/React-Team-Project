a30e5ca07d4633fe9a7670114616cf61bb6cf5fd
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-02-11 20:49:23 +0200

    add new Login/css code on Login.jsx page

--- forum/forum/src/views/Login/Login.jsx
+++ forum/forum/src/views/Login/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import { AppContext } from "../../Context/AppContext";
 import { loginUser } from "../../services/auth.service";
+import "./Login.css";
 
 export default function Login() {
   const { user, setContext } = useContext(AppContext);
@@ -34,16 +35,57 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <label htmlFor="email">Email: </label>
-      <input value={form.email} onChange={updateForm("email")} type="text" id="email" name="email"/>
-      <br /><br />
-      <label htmlFor="password">Password: </label>
-      <input value={form.password} onChange={updateForm("password")} type="text" id="password" name="password"/>
-      <br /><br />
-      <Button onClick={login}>Login</Button>
-      <p>Click{" "}<Link to="/register" style={{ textDecoration: "underline" }}>here</Link>{" "}if you don't have a registration.</p>
+    // <div class="login__form">
+    //   <h1 class="login__title">Login</h1>
+    //   <div class="login__box-input">
+    //     <input value={form.email} onChange={updateForm("email") } type="text" id="email" name="email" class="login__input" />
+    //     <label htmlFor="email" class="login__label">Email: </label>
+    //   </div>
+    //   <label htmlFor="password" >Password: </label>
+    //   <input value={form.password} onChange={updateForm("password")} type="text" id="password" name="password"/>
+    //   <br /><br />
+    //   <Button onClick={login}>Login</Button>
+    //   <p>Click{" "}<Link to="/register" style={{ textDecoration: "underline" }}>here</Link>{" "}if you don't have a registration.</p>
+    // </div>
+
+    <div className="login">
+      {/* <img src={loginBackground} alt="login background" className="login__background" /> */}
+      <div action="" className="login__form">
+        <h1 className="login__title">Login</h1>
+
+        <div className="login__content">
+          <div className="login__box">
+            <i className="ri-user-3-line login__icon"></i>
+
+            <div className="login__box-input">
+              <input value={form.email} onChange={updateForm("email")} type="text" id="email" name="email" className="login__input" />
+              <label htmlFor="email" className="login__label">Email: </label>
+            </div>
+          </div>
+
+          <div className="login__box">
+            <i className="ri-lock-2-line login__icon"></i>
+
+            <div className="login__box-input">
+              <input value={form.password} onChange={updateForm("password")} type="text" required className="login__input" id="password" name="password" />
+              <label htmlFor="password" className="login__label"> Password: </label>
+              <i className="ri-eye-off-line login__eye" id="login-eye"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="login__check">
+          <div className="login__check-group">
+            <input type="checkbox" className="login__check-input" id="login-check" />
+            <label type="login-check" className="login__check-label"> Remember me </label>
+          </div>
+        </div>
+
+        <Button onClick={login} className="login__button">Login</Button>
+        <p className="login__register">Click{" "}<Link to="/register" style={{ textDecoration: "underline" }}>here</Link>{" "}
+          if you don't have a registration.
+        </p>
+      </div>
     </div>
   );
 }
