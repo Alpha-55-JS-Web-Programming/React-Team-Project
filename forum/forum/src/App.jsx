91c03680fa20c4936fa813b2f3f7894e5422ce92
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-02-12 23:01:59 +0200

    changed Register added mobile and confirm password

--- forum/forum/src/App.jsx
+++ forum/forum/src/App.jsx
@@ -19,6 +19,7 @@ import { auth } from "./config/firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getUserData } from "./services/users.service";
 import ProfileInformation from "./views/ProfileInformation/ProfileInformation";
+// import  Admin from "./views/Login/Admin";
 
 function App() {
   const [context, setContext] = useState({
@@ -54,6 +55,7 @@ function App() {
           <Route path="/posts/:id" element={<Authenticated><SinglePost /></Authenticated>} />
           <Route path="/create-post" element={<Authenticated><CreatePost /></Authenticated>} />
           <Route path="/profile-information" element={<Authenticated><ProfileInformation /></Authenticated>} />
+          {/* <Route path="/admin" element={<Authenticated><Admin /></Authenticated>} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
--- forum/forum/src/views/Register/Register.jsx
+++ forum/forum/src/views/Register/Register.jsx
@@ -6,6 +6,7 @@ import { AppContext } from "../../Context/AppContext";
 import { useNavigate } from "react-router-dom";
 import "./Register.css";
 import { Link } from "react-router-dom";
+import phone from "../../../assets/phone.svg";
 
 export default function Register() {
   const { setContext } = useContext(AppContext);
@@ -13,15 +14,26 @@ export default function Register() {
     FullName: "",
     handle: "",
     email: "",
+    mobile: '',
     password: "",
+    confirmPassword: '',
   });
-  
+
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
   const updateForm = prop => e => {
     setForm({ ...form, [prop]: e.target.value });
   };
 
+  function validateDetails(mobile) {
+ 
+    const mobileRegex = /^\d{10}$/;
+    if (!mobileRegex.test(mobile)) {
+      setErrorMessage("Mobile number must be of 9 digits");
+      return false; // Show a err if the mobile number format is incorrect
+    }
+    return true;
+  }
   const register = async () => {
     // TODO: Validate inputs
     try {
@@ -35,7 +47,20 @@ export default function Register() {
         setErrorMessage("Please enter a valid email address.");
         return;
       }
-
+    //   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$/;
+  
+    // if (!passwordRegex.test(password)) {
+    //   setErrorMessage("Password error: Length must be 8 with 1 uppercase 1 lowercase and 1 special character");
+    //   return false; // Password doesn't meet the criteria.
+    // }
+  
+    if (form.password !== form.confirmPassword) {
+      setErrorMessage("Password and Confirm Password do not match");
+      return false; // Passwords do not match.
+    }
+      if(!validateDetails(form.mobile)){
+        setErrorMessage("Mobile number must be of 9 digits");
+      }
       const user = await getUserByHandle(form.handle);
       if (user.exists()) {
         setErrorMessage(`Handle @${form.handle} already exists`);
@@ -47,7 +72,8 @@ export default function Register() {
         form.FullName,
         form.handle,
         credentials.user.uid,
-        form.email
+        form.email,
+        form.mobile
       );
 
       setContext({ user: credentials.user, userData: null });
@@ -108,9 +134,26 @@ export default function Register() {
               <label htmlFor="password" className="login__label"> Password: </label>
               <i className="ri-eye-off-line login__eye" id="login-eye"></i>
             </div>
+            
+          </div>
+          <div className="login__box">
+            <i className="ri-lock-2-line login__icon"></i>
+
+            <div className="login__box-input">
+            <input value={form.confirmPassword} onChange={updateForm("confirmPassword")} type="text" required className="login__input" id="confirm-password" name="confirm-password" />
+              <label htmlFor="confirm-password" className="login__label"> confirm password: </label>
+              <i className="ri-eye-off-line login__eye" id="login-eye"></i>
+            </div>
+            
           </div>
         </div>
-    
+        <div>
+            <div>
+              <img src={phone} />
+            </div>
+            <input type="number" placeholder="Phone Number" name="mobile" value={form.mobile} onChange={updateForm("mobile")}/>
+          </div>
+          <br />
           <button onClick={register} className="login__button">Register</button><br/>
           <p className="login__register">Have an account?{" "}<Link to="/login" style={{ textDecoration: "underline" }}>Login</Link>{" "}</p>
       </div>
