d4a76e9795652ea77d1d90afd5669c4fa20a810c
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-02-15 16:45:56 +0200

    arrangement the code

--- forum/forum/src/views/AllPosts/AllPosts.jsx
+++ forum/forum/src/views/AllPosts/AllPosts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import PropTypes from "prop-types";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
-import { getAllPosts, dislikePost, likePost } from "../../services/post.services";
+import { getAllPosts, dislikePost, likePost} from "../../services/post.services";
 import { AppContext } from "../../Context/AppContext";
 import Sort from "../../components/Sort/Sort";
 import "./AllPosts.css";
@@ -22,27 +22,22 @@ export default function AllPosts() {
     switch (sortBy) {
       case "choose-sort":
         return setSortedPosts(posts);
-        case "most-liked":
-          sorted = [...posts].sort((a, b) => b.likedBy.length - a.likedBy.length);
-          return setSortedPosts(sorted);
-          case "most-commented":
-            sorted = Object.keys(posts).sort((a, b) => {
-                // Check if comments property exists for both posts
-                const commentsA = posts[a].comments ? Object.keys(posts[a].comments).length : 0;
-                const commentsB = posts[b].comments ? Object.keys(posts[b].comments).length : 0;
-                // Compare by the number of comments
-                return commentsB - commentsA;
-            });
-            // Convert the sorted keys back to an array of post objects
-            return setSortedPosts(sorted.map(postKey => posts[postKey]));
-            
-          case "newest":
-              sorted = [...posts].sort((a, b) => new Date(b.createdOnReadable) - new Date(a.createdOnReadable));
-              return setSortedPosts(sorted);
-    }
-    // Convert the sorted keys back to an array of post objects
-    // setSortedPosts(sorted.map(postKey => posts[postKey]));
-};
+      case "most-liked":
+        sorted = [...posts].sort((a, b) => b.likedBy.length - a.likedBy.length);
+        return setSortedPosts(sorted);
+      case "most-commented":
+        sorted = Object.keys(posts).sort((a, b) => {
+          const commentsA = posts[a].comments ? Object.keys(posts[a].comments).length : 0;
+          const commentsB = posts[b].comments ? Object.keys(posts[b].comments).length : 0;
+
+          return commentsB - commentsA;
+        });
+        return setSortedPosts(sorted.map((postKey) => posts[postKey]));
+      case "newest":
+        sorted = [...posts].sort((a, b) => new Date(b.createdOnReadable) - new Date(a.createdOnReadable));
+        return setSortedPosts(sorted);
+      }
+  };
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setSearch(value);
@@ -64,15 +59,14 @@ export default function AllPosts() {
   }, []);
 
   useEffect(() => {
-    const filteredPosts = posts.filter(post => post.title && post.title.toLowerCase().includes(search.toLowerCase()));
+    const filteredPosts = posts.filter((post) => post.title && post.title.toLowerCase().includes(search.toLowerCase()));
     setSortedPosts(filteredPosts);
   }, [posts, search]);
-
 
   const togglePostLike = (handle, postId) => {
     const postIndex = posts.findIndex((post) => post.id === postId);
     const post = posts[postIndex];
-  
+
     if (post.likedBy.includes(handle)) {
       dislikePost(handle, postId).then(() => {
         const updatedPosts = [...posts];
@@ -80,7 +74,7 @@ export default function AllPosts() {
           ...post,
           likedBy: post.likedBy.filter((u) => u !== handle),
         };
-  
+
         setPosts(updatedPosts);
       });
     } else {
@@ -90,7 +84,7 @@ export default function AllPosts() {
           ...post,
           likedBy: [...post.likedBy, handle],
         };
-  
+
         setPosts(updatedPosts);
       });
     }
@@ -98,41 +92,27 @@ export default function AllPosts() {
 
   return (
     <>
-    <div className="all-posts-container">
-      <h1>All posts</h1>
+      <div className="all-posts-container">
+        <h1>All posts</h1>
 
-      <div className="sort-search-container">
-        <Sort onSortChange={sortPosts} />
+        <div className="sort-search-container">
+          <Sort onSortChange={sortPosts} />
 
-        <label htmlFor="search">Search </label>
-        <input
-          value={search}
-          onChange={handleSearchChange}
-          type="text"
-          name="search"
-          id="search"
-          className="input-css"
-        />
-      </div>
-
-      {sortedPosts.map((post) => (
-        <div key={post.id}>
-          <h3>Title: {post.title}</h3>
-          <p> <strong>Context:</strong> {post.content} </p>
-          <p>{new Date(post.createdOnReadable).toLocaleDateString("bg-BG")}</p>
-          <Button onClick={() => togglePostLike(userData.handle, post.id)}>
-            {post.likedBy.includes(userData.handle) ? "Dislike" : "Like"}
-          </Button>
-          <Button onClick={() => navigate(`/posts/${post.id}`)}>
-            {" "}Details{" "}
-          </Button>
-          <br/><br/><br/>
+          <label htmlFor="search">Search </label>
+          <input value={search} onChange={handleSearchChange} type="text" name="search" id="search" className="input-css"/>
         </div>
-          
 
-      ))}
-      
-    </div>
+        {sortedPosts.map((post) => (
+          <div key={post.id}>
+            <h3>Title: {post.title}</h3>
+            <p> {" "} <strong>Context:</strong> {post.content}{" "} </p>
+            <p> {new Date(post.createdOnReadable).toLocaleDateString("bg-BG")} </p>
+            <Button onClick={() => togglePostLike(userData.handle, post.id)}> {post.likedBy.includes(userData.handle) ? "Dislike" : "Like"} </Button>
+            <Button onClick={() => navigate(`/posts/${post.id}`)}> {" "} Details{" "} </Button>
+            <br /><br /><br />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
