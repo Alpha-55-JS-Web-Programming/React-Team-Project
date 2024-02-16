20a6cb7b423c0d15e1012e380a16e8aef8f6f43c
Martin Andreev <xxdead_worldxx@abv.bg>
2024-02-16 18:21:36 +0200

    Fix: Add unique key props to list items in Trending component
    Resolved a React warning by ensuring each child in the list rendered by
    Trending.jsx has a unique "key" prop.

--- forum/forum/src/views/Trending/Trending.jsx
+++ forum/forum/src/views/Trending/Trending.jsx
@@ -6,14 +6,14 @@ const Trending = () => {
   const [topCommentedPosts, setTopCommentedPosts] = useState([]);
   const [mostRecentPosts, setMostRecentPosts] = useState([]);
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     const fetchTopCommentedPosts = async () => {
       const posts = await getTopCommentedPosts();
       console.log(posts);
       setTopCommentedPosts(posts);
     };
-    
+
     const fetchMostRecentPosts = async () => {
       const posts = await getMostRecentPosts();
       console.log(posts);
@@ -24,27 +24,33 @@ const Trending = () => {
     fetchMostRecentPosts();
   }, []);
 
+  // Correct approach to include key prop
   const renderPost = (post) => (
-    <>
-    <div key={post.id} className="post-item">
+    <div className="post-item">
       <h3>{post.title}</h3>
-      <p>{`Author: ${post.author }`}</p>
+      <p>{`Author: ${post.author}`}</p>
       <p>Created on: {new Date(post.createdOnReadable).toLocaleDateString('bg-BG')}</p>
       <p>{`Number of Likes: ${post.likedBy ? Object.keys(post.likedBy).length : 0}`}</p>
       <p>{`Number of Comments: ${post.comments ? Object.keys(post.comments).length : 0}`}</p>
-      {/* Add any other information you want to display */}
     </div>
-    <br />
-    </>
   );
 
+  // Now map with key prop at the correct location
   return (
     <div>
       <h2>Top 10 Most Commented Posts</h2>
-      {topCommentedPosts.map(renderPost)}
+      {topCommentedPosts.map(post => (
+        <React.Fragment key={post.id}>
+          {renderPost(post)}
+        </React.Fragment>
+      ))}
 
       <h2>10 Most Recently Created Posts</h2>
-      {mostRecentPosts.map(renderPost)}
+      {mostRecentPosts.map(post => (
+        <React.Fragment key={post.id}>
+          {renderPost(post)}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
