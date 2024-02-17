aa278b863781b880ba1fcc47bd04f95aec016a1b
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-02-17 21:06:56 +0200

    change the view of tags

--- forum/forum/src/components/Header/Header.jsx
+++ forum/forum/src/components/Header/Header.jsx
@@ -17,22 +17,25 @@ export default function Header() {
   return (
       <header className="sticky-header">
         <div className="round"> <img src={wheel} className="spin whalf" /></div>
-        <NavLink to="/"> <button className="btn btn1">Home</button> </NavLink>
-        <NavLink to="/trending"> <button className="btn btn2">Top 10 Trending</button> </NavLink>
-        <NavLink to="/latest"> <button className="btn btn1">Top 10 Latest</button> </NavLink>
-        <NavLink to="/allposts"> <button className="btn btn2">AllPosts</button> </NavLink>
-        { user && <NavLink to="/create-post"> <button className="btn btn1">Create Post</button> </NavLink>} 
-        {isAdmin && <NavLink to="/admin"> <button className="btn btn2">Admin</button> </NavLink>}
-        {user ? (
-        <>
-          <NavLink to="/profile-information" className="profile">{`Welcome, ${userData?.handle}`}</NavLink>
-        </>
-          ) : (
-        <>
-          <NavLink to="/login"><Button>Login</Button></NavLink>
-        </>
-        )}
-
+        <div className="header-main">
+          <NavLink to="/"> <button className="btn btn1">Home</button> </NavLink>
+          <NavLink to="/trending"> <button className="btn btn2">Top 10 Trending</button> </NavLink>
+          <NavLink to="/latest"> <button className="btn btn1">Top 10 Latest</button> </NavLink>
+          <NavLink to="/allposts"> <button className="btn btn2">AllPosts</button> </NavLink>
+          { user && <NavLink to="/create-post"> <button className="btn btn1">Create Post</button> </NavLink>} 
+          {isAdmin && <NavLink to="/admin"> <button className="btn btn2">Admin</button> </NavLink>}
+        </div>
+        <div className="login-wellcome">
+          {user ? (
+          <>
+            <NavLink to="/profile-information" className="login-wellcome">{`Welcome, ${userData?.handle}`}</NavLink>
+          </>
+            ) : (
+          <>
+            <NavLink to="/login"><button className="btn btn1">Login</button></NavLink>
+          </>
+          )}
+        </div>
       </header>
   );
 }
--- forum/forum/src/views/AllPosts/AllPosts.jsx
+++ forum/forum/src/views/AllPosts/AllPosts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import PropTypes from "prop-types";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
-import { getAllPosts, dislikePost, likePost,} from "../../services/post.services";
+import {getAllPosts, dislikePost, likePost} from "../../services/post.services";
 import { AppContext } from "../../Context/AppContext";
 import Sort from "../../components/Sort/Sort";
 import "./AllPosts.css";
@@ -30,13 +30,20 @@ export default function AllPosts() {
         return setSortedPosts(sorted);
       case "most-commented":
         sorted = Object.keys(posts).sort((a, b) => {
-          const commentsA = posts[a].comments ? Object.keys(posts[a].comments).length : 0;
-          const commentsB = posts[b].comments ? Object.keys(posts[b].comments).length : 0;
+          const commentsA = posts[a].comments
+            ? Object.keys(posts[a].comments).length
+            : 0;
+          const commentsB = posts[b].comments
+            ? Object.keys(posts[b].comments).length
+            : 0;
           return commentsB - commentsA;
         });
         return setSortedPosts(sorted.map((postKey) => posts[postKey]));
       case "newest":
-        sorted = [...posts].sort((a, b) => new Date(b.createdOnReadable) - new Date(a.createdOnReadable));
+        sorted = [...posts].sort(
+          (a, b) =>
+            new Date(b.createdOnReadable) - new Date(a.createdOnReadable)
+        );
         return setSortedPosts(sorted);
     }
   };
@@ -63,18 +70,25 @@ export default function AllPosts() {
 
   useEffect(() => {
     const filteredPosts = posts.filter((post) => {
-      const selectedTagNames = tags.filter((t) => t.selected).map(t => t.name);
-      const searchCriterion = !search || post.title?.toLowerCase().includes(search.toLowerCase());
-      const tagsCriterion = !selectedTagNames.length || post.tags?.some(t => selectedTagNames.includes(t));
+      const selectedTagNames = tags
+        .filter((t) => t.selected)
+        .map((t) => t.name);
+      const searchCriterion =
+        !search || post.title?.toLowerCase().includes(search.toLowerCase());
+      const tagsCriterion =
+        !selectedTagNames.length ||
+        post.tags?.some((t) => selectedTagNames.includes(t));
       return searchCriterion && tagsCriterion;
     });
     setSortedPosts(filteredPosts);
   }, [posts, search, tags]);
 
   useEffect(() => {
-    const uniqueTagNames = [... new Set(posts.flatMap(post => post.tags ?? []).filter(Boolean))]; // ["Weather", "Food", "Travel", ...]
+    const uniqueTagNames = [
+      ...new Set(posts.flatMap((post) => post.tags ?? []).filter(Boolean)),
+    ]; // ["Weather", "Food", "Travel", ...]
     // Our tags state has the format [{name: "Weather", selected: true, ...}], so we need to map the names to this format:
-    const tags = uniqueTagNames.map(name => ({name, selected: false}));
+    const tags = uniqueTagNames.map((name) => ({ name, selected: false }));
     setTags(tags);
   }, [posts]);
 
@@ -85,14 +99,20 @@ export default function AllPosts() {
     if (post.likedBy.includes(handle)) {
       dislikePost(handle, postId).then(() => {
         const updatedPosts = [...posts];
-        updatedPosts[postIndex] = {...post, likedBy: post.likedBy.filter((u) => u !== handle)};
+        updatedPosts[postIndex] = {
+          ...post,
+          likedBy: post.likedBy.filter((u) => u !== handle),
+        };
 
         setPosts(updatedPosts);
       });
     } else {
       likePost(handle, postId).then(() => {
         const updatedPosts = [...posts];
-        updatedPosts[postIndex] = {...post,likedBy: [...post.likedBy, handle]};
+        updatedPosts[postIndex] = {
+          ...post,
+          likedBy: [...post.likedBy, handle],
+        };
 
         setPosts(updatedPosts);
       });
@@ -100,7 +120,9 @@ export default function AllPosts() {
   };
 
   const handleTagSelect = (tag) => {
-    const updatedTags = tags.map((t) => t.name === tag.name ? {...t, selected: !t.selected}: t);
+    const updatedTags = tags.map((t) =>
+      t.name === tag.name ? { ...t, selected: !t.selected } : t
+    );
     setTags(updatedTags);
   };
 
@@ -108,45 +130,53 @@ export default function AllPosts() {
     <>
       <div>
         <h1 className="all-postsi-title">All posts</h1>
-
         <div className="sort-search-container">
           <Sort onSortChange={sortPosts} className="sort" />
-
           <div className="search-bar">
             <span className="material-symbols-outlined">search</span>
             <input value={search} placeholder="Search" onChange={handleSearchChange} type="text" name="search" id="search" className="input-css"/>
           </div>
         </div>
+
         <div className="tags-container">
           {tags.map((tag, i) => (
             <span key={`tag-${i}`} className={`tag ${tag.selected ? "selected" : ""}`} onClick={() => handleTagSelect(tag)}>{tag.name}</span>
           ))}
         </div>
+
         <div className="all-posts">
-         {sortedPosts.map((post) => (
-           <div className="post-id">
-             <div className="post-header">
-               <h2 className="post-author">{post.author}</h2>
-               <h3 className="post-title">{post.title}</h3>
-             </div>
-             <p className="post-created">{post.createdOnReadable}</p>
-             {post.tags?.length ? (
+          {sortedPosts.map((post) => (
+            <div className="post-id">
+  
+              <div className="post-header">
+                <h2 className="post-author">{post.author}</h2>
+                <p className="post-created">{post.createdOnReadable}</p>
+              </div>
+
+              {post.tags?.length ? (
                <p><strong>Tags:</strong> {post.tags?.join(", ")}</p>
-             ) : null}
-             <p><strong>Context:</strong> {post.content}</p>
-             <div className="post-actions">
-               <span className="material-symbols-outlined thumb-icon" onClick={() => togglePostLike(userData.handle, post.id)}>
-                 {" "}thumb_up{" "} <span className="like-count"> {" "} {post.likedBy ? Object.keys(post.likedBy).length : 0} </span>
-               </span>
-               <Button onClick={() => navigate(`/posts/${post.id}`)}> Details </Button>
-             </div>
-           </div>
-         ))}
+              ) : null}
+              
+              <div className="post-body">
+               <h3 className="post-title">{post.title}</h3>
+               <p className="post-content"><strong>Context:</strong> {post.content}</p>
+              </div>
+  
+              <div className="post-actions">
+                <div className="like-section">
+                  <span className="material-symbols-outlined thumb-icon" onClick={() => togglePostLike(userData.handle, post.id)}>
+                    {" "}thumb_up{" "} <span className="like-count"> {" "} {post.likedBy ? Object.keys(post.likedBy).length : 0} </span>
+                  </span>
+                </div>
+                <button onClick={() => navigate(`/posts/${post.id}`)} className="button-details"> Details </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
   );
-}
+  }
 
 AllPosts.propTypes = {
   post: PropTypes.shape({
