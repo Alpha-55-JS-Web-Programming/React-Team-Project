e257a73a453eca0bb18b6a8c7006962bf8b643c5
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-02-19 14:51:37 +0200

    post.content.slice added

--- forum/forum/src/views/Home/Home.jsx
+++ forum/forum/src/views/Home/Home.jsx
@@ -185,6 +185,8 @@ export default function Home() {
               <div className="post-body">
                <h3 className="post-title">{post.title}</h3>
                <p className="post-content">{post.content}</p>
+                {/* <p className="post-content">{post.content.length > 200 ? post.content.slice(0, 200) + '...' : post.content}</p> */}
+
               </div>
 
               <div className="post-actions">
