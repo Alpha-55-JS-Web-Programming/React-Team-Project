68bcc1be6686d74d83ee9b28421a32457fc2e11f
Martin Andreev <xxdead_worldxx@abv.bg>
2024-02-20 17:18:10 +0200

    Change the tags
    Now we have different tags with different names

--- forum/forum/src/components/Sort/TagPicker.jsx
+++ forum/forum/src/components/Sort/TagPicker.jsx
@@ -14,10 +14,11 @@ export default function TagPicker({ onTagChange }) {
         <div className="tag-container">
             <select className="tag-select" onChange={handleTagChange} name="tag" id="tag" value={tag}>
                 <option value="">Choose a tag:</option>
-                <option value="food">Food</option>
-                <option value="exercise">Exercise</option>
-                <option value="rest">Rest</option>
-                <option value="mental-health">Mental health</option>
+                <option value="food">Nutrition and Diet</option>
+                <option value="exercise">Physical Fitness</option>
+                <option value="rest">Mental Well-being</option>
+                <option value="mental-health">Lifestyle and Habits</option>
+                <option value="mental-health">Holistic Health</option>
             </select>
         </div>
     );
